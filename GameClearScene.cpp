d061db3b1513d839d4c201a44ac1a42d383f0055
雅基 高野 <220034@jc-21.jp>
2024-09-09 13:02:41 +0900

    謎エラー出現

--- GameClearScene.cpp
+++ GameClearScene.cpp
@@ -6,7 +6,7 @@
 
 //コンストラクタ
 GameClearScene::GameClearScene(GameObject* parent)
-	: GameObject(parent, "GameClearScene"), hBackGround_(-1), hClear_(-1), BGM_(-1),
+	: GameObject(parent, "GameClearScene"), hImage_{ 0, 1, 2 }, BGM_(-1),
 	frameCount(0), DelayFrame(120), alphaSpeed(0.02f), alphaDir(1)
 {
 }
@@ -14,19 +14,49 @@ GameClearScene::GameClearScene(GameObject* parent)
 //初期化
 void GameClearScene::Initialize()
 {
-	//画像データのロード
+	/*画像データのロード
 	hBackGround_ = Image::Load("GameClearBackGround.png");
 	assert(hBackGround_ >= IMB);
 
-	hClear_ = Image::Load("GameClear.png");
-	assert(hClear_ >= IMB);
+	hClearLogo_ = Image::Load("GameClear.png");
+	assert(hClearLogo_ >= IMB);
+
+	hLight_ = Image::Load("EffectLight.png");
+	assert(hLight_ >= IMB);
+	*/
+
+	const char* ImageNames[IMAGE_MAX] = { "GameClearBackGround.png", "GameClear.png", "EffectLight.png" };
+	for (int i = 0; i < IMAGE_MAX; i++)
+	{
+		hImage_[i] = Image::Load(ImageNames[i]);
+		assert(hImage_[i] > IMB);
+
+		switch (i)
+		{
+		case BACKGROUND:
+			break;
+		case CLEAR_LOGO:
+			tImage_[i].position_ = CLEAR_LOGO_POS;
+			tImage_[i].scale_ = CLEAR_LOGO_SCL;
+			break;
+		case LIGHT_LOGO:
+			tImage_[i].position_ = LIGHT_LOGO_POS;
+			tImage_[i].scale_ = LIGHT_LOGO_SCL;
+			tImage_[i].alpha_ = 1.0f;
+			break;
+		}
+	}
 
 	BGM_ = Audio::Load("GameClearBGM.wav");
 	assert(BGM_ >= IMB);
 
-	tClear.position_ = CLEAR_LOGO_POS;
+	/*tClear.position_ = CLEAR_LOGO_POS;
 	tClear.scale_ = CLEAR_LOGO_SCL;
-	tClear.alpha_ = 1.0f;
+
+	tLight.position_ = LIGHT_LOGO_POS;
+	tLight.scale_ = LIGHT_LOGO_SCL;
+	tLight.alpha_ = 1.0f;
+	*/
 }
 
 //更新
@@ -34,14 +64,15 @@ void GameClearScene::Update()
 {
 	Audio::Play(BGM_);
 
+	/*
 	// アルファ値の増減処理
-	tClear.alpha_ += alphaDir * alphaSpeed;
-	if (tClear.alpha_ >= 1.0f) {
-		tClear.alpha_ = 1.0f;
+	tLight.alpha_ += alphaDir * alphaSpeed;
+	if (tLight.alpha_ >= 1.0f) {
+		tLight.alpha_ = 1.0f;
 		alphaDir = -1;  // 減少方向に変更
 	}
-	else if (tClear.alpha_ <= 0.0f) {
-		tClear.alpha_ = 0.0f;
+	else if (tLight.alpha_ <= 0.0f) {
+		tLight.alpha_ = 0.0f;
 		alphaDir = 1;  // 増加方向に変更
 	}
 
@@ -49,8 +80,42 @@ void GameClearScene::Update()
 
 	/// 拡大縮小の変化（強調）
 	float scaleVariation = 0.1f * sinf(static_cast<float>(frameCount) / 20.0f);
-	tClear.scale_.x = CLEAR_LOGO_SCL.x + scaleVariation;
-	tClear.scale_.y = CLEAR_LOGO_SCL.y + scaleVariation;
+	tLight.scale_.x = CLEAR_LOGO_SCL.x + scaleVariation;
+	tLight.scale_.y = CLEAR_LOGO_SCL.y + scaleVariation;
+	*/
+
+	for (int i = 0; i < IMAGE_MAX; i++)
+	{
+		switch (i)
+		{
+		case BACKGROUND:
+			break;
+		case CLEAR_LOGO:
+			break;
+		case LIGHT_LOGO:
+			// エフェクトライトの処理（例：アルファ値を変化させる）
+			tImage_[i].alpha_ += alphaDir * alphaSpeed;
+			if (tImage_[i].alpha_ >= 1.0f) {
+				tImage_[i].alpha_ = 1.0f;
+				alphaDir = -1;  // 減少方向に変更
+			}
+			else if (tImage_[i].alpha_ <= 0.0f) {
+				tImage_[i].alpha_ = 0.0f;
+				alphaDir = 1;  // 増加方向に変更
+			}
+
+			// 拡大縮小の変化（強調）
+			float scaleVariation = 0.1f * sinf(static_cast<float>(frameCount) / 20.0f);
+			tImage_[i].scale_.x = CLEAR_LOGO_SCL.x + scaleVariation;
+			tImage_[i].scale_.y = CLEAR_LOGO_SCL.y + scaleVariation;
+
+			break;
+		}
+	}
+
+	// フレームカウントの更新
+	frameCount = (frameCount + 1) % 360;
+
 
 	if (frameCount >= DelayFrame)
 	{
@@ -75,11 +140,21 @@ void GameClearScene::Update()
 //描画
 void GameClearScene::Draw()
 {
-	Image::SetTransform(hBackGround_, transform_);
+	/*Image::SetTransform(hBackGround_, transform_);
 	Image::Draw(hBackGround_);
 
-	Image::SetTransform(hClear_, tClear);
-	Image::Draw(hClear_);
+	Image::SetTransform(hLight_, tLight);
+	Image::Draw(hLight_);
+
+	Image::SetTransform(hClearLogo_, tClear);
+	Image::Draw(hClearLogo_);
+	*/
+
+	for (int i = 0; i < IMAGE_MAX; i++)
+	{
+		Image::SetTransform(hImage_[i], tImage_[i]);
+		Image::Draw(hImage_[i]);
+	}
 }
 
 //開放
--- GameClearScene.h
+++ GameClearScene.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "Engine/GameObject.h"
 
+enum ImageID {
+	BACKGROUND,
+	CLEAR_LOGO,
+	LIGHT_LOGO,
+	IMAGE_MAX
+};
+
 class GameClearScene : public GameObject
 {
-	int hBackGround_;
-	int hClear_;
+	int hImage_[IMAGE_MAX];
+	//int hBackGround_;
+	//int hClearLogo_;
+	//int hLight_;
 	int BGM_;
 	int IMB = 0; //I=画像、M=モデル、B=BGM
 
@@ -13,13 +22,20 @@ class GameClearScene : public GameObject
 	XMFLOAT3 CLEAR_LOGO_SCL = XMFLOAT3(1.5f, 1.5f, 1.5f);
 	//------------------------------------------------
 
+	//-----ロゴの後光の位置、大きさ、透明度の設定-----
+	XMFLOAT3 LIGHT_LOGO_POS = XMFLOAT3(-0.02f, 0.35f, 0.0f);
+	XMFLOAT3 LIGHT_LOGO_SCL = XMFLOAT3(1.1f, 1.1f, 1.1f);
+	//------------------------------------------------
+
 	int alphaDir;
 	float alphaSpeed;
 
 	unsigned int frameCount;
 	unsigned int DelayFrame;
 
-	Transform tClear;
+	//Transform tLight;
+	//Transform tClear;
+	Transform tImage_[IMAGE_MAX];
 public:
 	//コンストラクタ
 	//引数：parent  親オブジェクト（SceneManager）
